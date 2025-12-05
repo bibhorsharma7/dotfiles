4fda96fad98bdb3879bfbf6305db5cb9e6fbcb6c
Bibhor Sharma <bibhorsharma7@gmail.com>
2025-12-05 17:26:36 +1100

    add git keymaps

--- .config/nvim/lua/plugins/fugitive.lua
+++ .config/nvim/lua/plugins/fugitive.lua
@@ -3,5 +3,7 @@ return {
 	event = { "BufReadPre", "BufNewFile" },
 	config = function()
 		vim.keymap.set("n", "<leader>gs", "<cmd>Git<CR>", { desc = "Open git status" })
+		vim.keymap.set("n", "<leader>gb", "<cmd>Git blame<CR>", { desc = "Open git blame" })
+		vim.keymap.set("n", "<leader>gl", "<cmd>Git log<CR>", { desc = "Open git log" })
 	end,
 }
